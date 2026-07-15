d9fd264637bf184732cbece24ba1e9d87c83420b
vincepeng518 <152611245+vincepeng518@users.noreply.github.com>
2026-07-15 23:48:16 +0800

    cleanup test 81a1eec6b9b6

--- strategies/user/81a1eec6b9b6.py
+++
@@ -1,9 +0,0 @@
-from strategies.base import Bar, Signal, StrategyBase
-class PersistTest(StrategyBase):
-    name = "persist_test"
-    description = "persistence e2e"
-    category = "test"
-    def init(self, p):
-        super().init(p)
-    def next(self, bar):
-        return None
